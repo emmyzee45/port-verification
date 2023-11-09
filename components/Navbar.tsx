998362ec88d3f7d7e8cf2619e0c179ae8b6ace3e
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-11-09 13:50:56 +0100

    fix: use Image and Link instead

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -133,7 +133,7 @@ const Navbar = () => {
                   >
                     <span className="absolute -inset-1.5"></span>
                     <span className="sr-only">Open user menu</span>
-                    <img
+                    <Image
                       className="h-8 w-8 rounded-full"
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                       alt=""
@@ -183,31 +183,31 @@ const Navbar = () => {
 
         <div className="sm:hidden" id="mobile-menu">
           <div className="space-y-1 px-2 pb-3 pt-2">
-            <a
+            <Link
               href="/"
               className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
               aria-current="page"
             >
               Dashboard
-            </a>
-            <a
+            </Link>
+            <Link
               href="/"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
             >
               Team
-            </a>
-            <a
+            </Link>
+            <Link
               href="/"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
             >
               Projects
-            </a>
-            <a
+            </Link>
+            <Link
               href="/"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
             >
               Calendar
-            </a>
+            </Link>
           </div>
         </div>
       </nav>
